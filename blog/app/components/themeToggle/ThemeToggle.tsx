5aa08c69db215bfd260006cd74852b8003f94722
yashu sthapit <yashusthapit@gmail.com>
2024-05-19 11:25:24 +0545

    created the darkmode button with responsive navbar

--- blog/app/components/themeToggle/ThemeToggle.tsx
+++ blog/app/components/themeToggle/ThemeToggle.tsx
@@ -10,13 +10,26 @@ const moon = "https://cdn-icons-png.flaticon.com/512/414/414942.png";
 const sun = "https://cdn-icons-png.flaticon.com/512/169/169367.png";
 
 const ThemeToggle = () => {
-  const { theme }: any = useContext(ThemeContext);
+  const { theme, toggle }: any = useContext(ThemeContext);
   console.log(theme);
 
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      onClick={toggle}
+      style={
+        theme === "dark" ? { backgroundColor: "white" } : { backgroundColor: "#0f172a" }
+      }
+    >
       <Image src={moon} width={14} height={14} alt="moon_png" />
-      <div className={styles.ball}></div>
+      <div
+        className={styles.ball}
+        style={
+          theme === "dark"
+            ? { left: 1, backgroundColor: "#0f172a" }
+            : { right: 1, backgroundColor: "white" }
+        }
+      ></div>
       <Image src={sun} width={14} height={14} alt="moon_png" />
     </div>
   );
